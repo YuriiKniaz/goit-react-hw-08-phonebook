59321ac219ca365ff891cf6ee9f95d835dae7770
YuriiKniaz <127882850+YuriiKniaz@users.noreply.github.com>
2023-11-07 10:24:00 +0000

    Deploying to gh-pages from @ YuriiKniaz/goit-react-hw-08-phonebook@26475eea4fbe8ab358d4ce69443ed9a7ad86ab36 🚀

---
+++ static/js/983.a416b03e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[983],{3983:function(o,e,n){n.r(e);n(2791);var t=n(184);e.default=function(){return(0,t.jsx)("div",{children:"Welcome to your PhoneBook"})}}}]);
+//# sourceMappingURL=983.a416b03e.chunk.js.map
